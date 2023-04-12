73cb6c6329b01acd588513c3809a6b45d39fbb68
Gui Silva <adrianon10@gmail.com>
2023-04-12 03:06:00 -0300

    feat: add server config

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,28 @@
-console.log('testando')
+import { Server } from '@overnightjs/core'
+import express, { type Application } from 'express'
+
+import { ForecastController } from './controllers/forecast'
+
+export class SetupServer extends Server {
+  constructor(private readonly port = 3333 || process.env.PORT) {
+    super()
+  }
+
+  public init (): void {
+    this.setupExpress()
+    this.setupControllers()
+  }
+
+  public getApp (): Application {
+    return this.app
+  }
+
+  private setupExpress (): void {
+    this.app.use(express.json())
+  }
+
+  private setupControllers (): void {
+    const forecastController = new ForecastController()
+    this.addControllers([forecastController])
+  }
+}
