17879251e851ef6e4c772698e4dc79adba62cd43
Gui Silva <adrianon10@gmail.com>
2023-04-24 04:35:41 -0300

    test: it should return 422 when there is a validation error

--- test/functional/beaches.spec.ts
+++ test/functional/beaches.spec.ts
@@ -16,5 +16,20 @@ describe('#Beaches functional tests', () => {
       expect(response.status).toBe(201)
       expect(response.body).toEqual(expect.objectContaining(newBeach))
     })
+
+    it('should return 422 when there is a validation error', async () => {
+      const newBeach = {
+        name: 'Manly',
+        position: 'E',
+        latitude: 'invalid_latitude',
+        longitude: 151.289824
+      }
+      const response = await global.testRequest.post('/beaches').send(newBeach)
+
+      expect(response.status).toBe(422)
+      expect(response.body).toEqual({
+        error: 'Beach validation failed: latitude: Cast to Number failed for value "invalid_latitude" (type string) at path "latitude"'
+      })
+    })
   })
 })
