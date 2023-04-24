b8369fbb8590e0ad64ae06bf352b17dffcf570b8
Gui Silva <adrianon10@gmail.com>
2023-04-24 04:14:46 -0300

    feat: add helper method create

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,10 +1,14 @@
 import { Controller, Post } from '@overnightjs/core'
 import { Request, Response } from 'express'
 
+import { Beach } from '@src/models/beaches-model'
+
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async create (request: Request, response: Response): Promise<Response> {
-    return response.status(201).send({ ...request.body, id: 'fake_id' })
+    const beach = new Beach(request.body)
+    const result = await beach.save()
+    return response.status(201).send(result)
   }
 }
