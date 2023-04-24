20f71ee8845831d090403387611717e2e4dc53d0
Gui Silva <adrianon10@gmail.com>
2023-04-24 04:36:11 -0300

    feat: add try/catch to handle errors

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,5 +1,6 @@
 import { Controller, Post } from '@overnightjs/core'
 import { Request, Response } from 'express'
+import mongoose from 'mongoose'
 
 import { Beach } from '@src/models/beaches-model'
 
@@ -7,8 +8,21 @@ import { Beach } from '@src/models/beaches-model'
 export class BeachesController {
   @Post('')
   public async create (request: Request, response: Response): Promise<Response> {
-    const beach = new Beach(request.body)
-    const result = await beach.save()
-    return response.status(201).send(result)
+    try {
+      const beach = new Beach(request.body)
+      const result = await beach.save()
+      return response.status(201).send(result)
+    } catch (err) {
+      if (err instanceof mongoose.Error.ValidationError) {
+        const error = err as Error
+        return response.status(422).send({
+          error: error.message
+        })
+      } else {
+        return response.status(500).send({
+          error: 'Internal Server Error'
+        })
+      }
+    }
   }
 }
