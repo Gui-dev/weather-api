263dbc4d936f88601b10a094969c89fd71a73c80
Gui Silva <adrianon10@gmail.com>
2023-04-25 04:39:38 -0300

    feat: add forecast controller

---
+++ src/controllers/forecast.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@overnightjs/core'
+import { Request, Response } from 'express'
+
+import { ForecastService } from '@src/services/forecast-service'
+import { Beach } from '@src/models/beaches-model'
+
+@Controller('forecast')
+export class ForecastController {
+  @Get('')
+  public async getForecastForLoggedUser (request: Request, response: Response): Promise<Response> {
+    const forecast = new ForecastService()
+    const beaches = await Beach.find({})
+    const forecastData = await forecast.processForecastForBeaches(beaches)
+
+    return response.status(200).send(forecastData)
+  }
+}
