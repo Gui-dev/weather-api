5cc30806681da9b47ef44ca9977b196fb76014b2
Gui Silva <adrianon10@gmail.com>
2023-04-28 03:34:01 -0300

    feat: add helper method sendCreatedUpdateErrorResponse

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,14 +1,21 @@
 import { Controller, Post } from '@overnightjs/core'
 import { Request, Response } from 'express'
+import type mongoose from 'mongoose'
 
 import { User } from '@src/models/user-model'
+import { BaseController } from '.'
 
 @Controller('users')
-export class UsersController {
+export class UsersController extends BaseController {
   @Post('')
-  public async create (request: Request, response: Response): Promise<Response> {
-    const user = new User(request.body)
-    const newUser = await user.save()
-    return response.status(201).send(newUser)
+  public async create (request: Request, response: Response): Promise<Response | any> {
+    try {
+      const user = new User(request.body)
+      const newUser = await user.save()
+      return response.status(201).send(newUser)
+    } catch (err) {
+      const error = err as mongoose.Error.ValidationError | Error
+      this.sendCreatedUpdateErrorResponse(response, error)
+    }
   }
 }
