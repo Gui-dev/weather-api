4eb0a5aa11c99baee3c7471995b0b06694df6a45
Gui Silva <adrianon10@gmail.com>
2023-04-28 04:13:36 -0300

    feat: add helper method handleClientErrors

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,21 +1,46 @@
 import { type Response } from 'express'
 import mongoose from 'mongoose'
 
+import { CUSTOM_VALIDATION } from '@src/models/user-model'
+
+interface IHandleClientErrorsResponse {
+  code: number
+  error: string
+}
+
 export abstract class BaseController {
   protected sendCreatedUpdateErrorResponse (
     response: Response,
     error: mongoose.Error.ValidationError | Error
-  ): Response {
+  ): void {
     if (error instanceof mongoose.Error.ValidationError) {
-      return response.status(422).send({
-        code: 422,
-        error: error.message
+      const clientError = this.handleClientErrors(error)
+      response.status(clientError.code).send({
+        code: clientError.code,
+        error: clientError.error
       })
     } else {
-      return response.status(500).send({
+      response.status(500).send({
         code: 500,
         error: 'Something went wrong'
       })
+    }
+  }
+
+  private handleClientErrors (error: mongoose.Error.ValidationError): IHandleClientErrorsResponse {
+    const duplicatedKindError = Object.values(error.errors).filter(
+      err => err.kind === CUSTOM_VALIDATION.DUPLICATED
+    )
+    if (duplicatedKindError.length) {
+      return {
+        code: 409,
+        error: error.message
+      }
+    } else {
+      return {
+        code: 422,
+        error: error.message
+      }
     }
   }
 }
