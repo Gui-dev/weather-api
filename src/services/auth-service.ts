d66aa5da1a9aa366e1a82e9fcbbe1213d15a2a61
Gui Silva <adrianon10@gmail.com>
2023-05-01 04:08:59 -0300

    feat: add helper method generateToken

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export class AuthService {
   public static async hashPassword (password: string, salt = 10): Promise<string> {
@@ -9,5 +10,12 @@ export class AuthService {
   public static async comparePassword (password: string, passwordHashed: string): Promise<boolean> {
     const isValidPassword = await bcrypt.compare(password, passwordHashed)
     return isValidPassword
+  }
+
+  public static generateToken (payload: Record<string, string>): string {
+    const token = jwt.sign(payload, 'secretword', {
+      expiresIn: '7d'
+    })
+    return token
   }
 }
