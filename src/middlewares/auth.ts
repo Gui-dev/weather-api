795aee179657b09d16e6e5257cb3821a3da4d986
Gui Silva <adrianon10@gmail.com>
2023-05-04 03:40:43 -0300

    feat: add handling error if there is a problem on the token verification

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,9 +2,16 @@ import { AuthService } from '@src/services/auth-service'
 import { type Response, type Request, type NextFunction } from 'express'
 
 export const authMiddleware = (request: Partial<Request>, response: Partial<Response>, next: NextFunction): void => {
-  const token = request.headers?.['x-access-token']
-  const decoded = AuthService.decodeToken(token as string)
-  request.decoded = decoded
-
-  next()
+  try {
+    const token = request.headers?.['x-access-token']
+    const decoded = AuthService.decodeToken(token as string)
+    request.decoded = decoded
+    next()
+  } catch (err) {
+    const error = err as Error
+    response.status?.(401).send({
+      code: 401,
+      error: error.message
+    })
+  }
 }
