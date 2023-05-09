7689c11dc69121873d933244ecdcaea2fc80711a
Gui Silva <adrianon10@gmail.com>
2023-05-09 03:18:29 -0300

    feat: add handling error when shutting down the app

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import logger from './config/logger'
 import { SetupServer } from './server'
+
+enum ExitStatus {
+  Failure = 1,
+  Success = 0
+}
 
 const bootstrap = async (): Promise<void> => {
   const server = new SetupServer()
   await server.init()
   server.start()
+  const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT']
+  exitSignals.map(sig => process.on(sig, async () => {
+    try {
+      await server.close()
+      logger.info('App exited with success')
+      process.exit(ExitStatus.Success)
+    } catch (error) {
+      logger.error(`App exited with error ${error}`)
+      process.exit(ExitStatus.Failure)
+    }
+  }))
 }
 
 bootstrap()
   .catch(error => {
-    console.log(error)
+    logger.error(`App exited with error ${error}`)
+    process.exit(ExitStatus.Failure)
   })
