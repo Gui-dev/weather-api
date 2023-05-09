9fef703ef2b32836942331afbd13430a7722c084
Gui Silva <adrianon10@gmail.com>
2023-05-09 03:26:07 -0300

    feat: add unhandledRejection and uncaughtException process

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-base-to-string */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import logger from './config/logger'
 import { SetupServer } from './server'
@@ -6,6 +7,18 @@ enum ExitStatus {
   Failure = 1,
   Success = 0
 }
+
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error(
+    `App exiting due to an unhandled promise: ${promise} and reason: ${reason}`
+  )
+  throw reason
+})
+
+process.on('uncaughtException', error => {
+  logger.error(`App exiting due to an uncaught exception: ${error}`)
+  process.exit(ExitStatus.Failure)
+})
 
 const bootstrap = async (): Promise<void> => {
   const server = new SetupServer()
