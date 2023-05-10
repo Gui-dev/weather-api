448ece70cb92739b079b794d3126d6be984e0abc
Gui Silva <adrianon10@gmail.com>
2023-05-10 03:49:23 -0300

    refactor: remove type

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,9 +1,9 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import * as dotenv from 'dotenv'
-import { type User } from '@src/models/user-model'
+import { type IUser } from '@src/models/user-model'
 
-export interface IDecodedUser extends Omit<typeof User, '_id'> {
+export interface IDecodedUser extends Omit<IUser, '_id'> {
   id: string
 }
 dotenv.config()
@@ -20,8 +20,8 @@ export class AuthService {
   }
 
   public static generateToken (payload: Record<string, string>): string {
-    const secret = process.env.AUTH_SECRET_WORD as string
-    const expiresIn = process.env.AUTH_EXPIRES_IN as string
+    const secret = process.env.AUTH_SECRET_WORD
+    const expiresIn = process.env.AUTH_EXPIRES_IN
     const token = jwt.sign(payload, secret, {
       expiresIn
     })
@@ -29,7 +29,7 @@ export class AuthService {
   }
 
   public static decodeToken (token: string): IDecodedUser {
-    const secret: string = process.env.AUTH_SECRET_WORD as string
+    const secret: string = process.env.AUTH_SECRET_WORD
     return jwt.verify(token, secret) as IDecodedUser
   }
 }
