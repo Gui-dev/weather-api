a5b78a34ae57161ec5e8af066490d85da6066bd2
Gui Silva <adrianon10@gmail.com>
2023-05-11 03:31:58 -0300

    feat: add helper method isWindOffShore

--- src/services/rating-service.ts
+++ src/services/rating-service.ts
@@ -1,4 +1,4 @@
-import { type IBeach, type BeachPosition } from '@src/models/beaches-model'
+import { type IBeach, BeachPosition } from '@src/models/beaches-model'
 
 export class RatingService {
   constructor(private readonly beach: IBeach) { }
@@ -6,8 +6,35 @@ export class RatingService {
   public async getRatingBasedOnWindAndWavePosition (wavePosition: BeachPosition, windPosition: BeachPosition): Promise<number> {
     if (wavePosition === windPosition) {
       return 1
+    } else if (this.isWindOffShore(wavePosition, windPosition)) {
+      return 5
     }
 
-    return 0
+    return 3
+  }
+
+  private isWindOffShore (wavePosition: BeachPosition, windPosition: BeachPosition): boolean {
+    return (
+      (
+        wavePosition === BeachPosition.N &&
+        windPosition === BeachPosition.S &&
+        this.beach.position === BeachPosition.N
+      ) ||
+      (
+        wavePosition === BeachPosition.S &&
+        windPosition === BeachPosition.N &&
+        this.beach.position === BeachPosition.S
+      ) ||
+      (
+        wavePosition === BeachPosition.E &&
+        windPosition === BeachPosition.W &&
+        this.beach.position === BeachPosition.E
+      ) ||
+      (
+        wavePosition === BeachPosition.W &&
+        windPosition === BeachPosition.E &&
+        this.beach.position === BeachPosition.W
+      )
+    )
   }
 }
