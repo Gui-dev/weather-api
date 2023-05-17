e0be8097f8db38856853eeee35a1e5295e8f9879
Gui Silva <adrianon10@gmail.com>
2023-05-17 03:11:11 -0300

    test: it should format error with mandatory fields and description

--- src/errors/__test__/api-errors.spec.ts
+++ src/errors/__test__/api-errors.spec.ts
@@ -13,4 +13,19 @@ describe('#API Errors', () => {
       message: 'User not found'
     })
   })
+
+  it('should format error with mandatory fields and description', async () => {
+    const error = APIError.format({
+      code: 404,
+      message: 'User not found',
+      description: 'This error happens when there is no user created'
+    })
+
+    expect(error).toEqual({
+      code: 404,
+      error: 'Not Found',
+      message: 'User not found',
+      description: 'This error happens when there is no user created'
+    })
+  })
 })
