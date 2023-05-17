4f0332e723275c6bb0906ea62181fdc27135d72d
Gui Silva <adrianon10@gmail.com>
2023-05-17 03:23:07 -0300

    chore: add new flags

--- src/shared/database/database.ts
+++ src/shared/database/database.ts
@@ -1,7 +1,7 @@
 import mongoose, { type Mongoose } from 'mongoose'
 
 export const connect = async (): Promise<Mongoose> => {
-  return await mongoose.connect(process.env.DATABASE_URL as string)
+  return await mongoose.connect(process.env.DATABASE_URL)
 }
 
 export const close = async (): Promise<void> => {
